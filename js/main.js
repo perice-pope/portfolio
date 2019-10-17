76fa2663249120940abbfdd528bc91fc2df255a8
Perice Pope <perice09@gmail.com>
2019-10-17 14:41:39 -0700

    fix nav about code buttons

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-$(document).ready(function(){
-    $('.parallax').parallax();
-  });
+$(document).ready(function () {
 
-//   alert('i work!'); 
+  $('.first-button').on('click', function () {
+
+    $('.animated-icon1').toggleClass('open');
+  });
+  alert('hey!'); 
+  
+  $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+  });
+  $('.third-button').on('click', function () {
+
+    $('.animated-icon3').toggleClass('open');
+  });
+});
